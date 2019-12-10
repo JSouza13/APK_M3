b4d93fb99abab7304bea29dd2364777a9e10cb60
bryandantas1 <30815201+bryandantas1@users.noreply.github.com>
2019-12-10 20:06:14 -0300

    Adicionado visualização de foto na listagem de pontos

--- app/src/main/java/com/example/guardaticket/adapter/PontoViewHolder.java
+++ app/src/main/java/com/example/guardaticket/adapter/PontoViewHolder.java
@@ -1,6 +1,8 @@
 package com.example.guardaticket.adapter;
 
+import android.net.Uri;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -11,12 +13,14 @@ import com.example.guardaticket.R;
 import com.example.guardaticket.RegistroPontos;
 import com.example.guardaticket.model.PontoModel;
 
+import java.net.URI;
 import java.text.DateFormat;
 
 public class PontoViewHolder extends RecyclerView.ViewHolder {
     private TextView data;
     private TextView hora;
     private ConstraintLayout clPai;
+    private ImageView image;
     private String idPonto;
 
     public PontoViewHolder(@NonNull View itemView){
@@ -31,6 +35,8 @@ public class PontoViewHolder extends RecyclerView.ViewHolder {
 
         data = itemView.findViewById(R.id.textData);
         hora = itemView.findViewById(R.id.textHora);
+        image = itemView.findViewById(R.id.imageTicket);
+
 
         clPai = (ConstraintLayout) itemView;
 
@@ -42,6 +48,10 @@ public class PontoViewHolder extends RecyclerView.ViewHolder {
         DateFormat formatter = android.text.format.DateFormat.getDateFormat(data.getContext());
         String dataFormatada = formatter.format(ponto.getDate().getTime());
         data.setText(dataFormatada);
+
+        if(ponto.getFoto() != null){
+            image.setImageURI(Uri.parse(ponto.getFoto()));
+        }
 
         hora.setText(ponto.getHoras() + ":" + ponto.getMinutos());
 
