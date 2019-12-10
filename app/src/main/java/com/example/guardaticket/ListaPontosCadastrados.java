b7456c432eac6d31b011fbae594c8627312b5242
João Souza <jooao_souza@hotmail.com>
2019-12-10 18:29:23 -0300

    Ajustados telas de registros

---
+++ app/src/main/java/com/example/guardaticket/ListaPontosCadastrados.java
@@ -0,0 +1,14 @@
+package com.example.guardaticket;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListaPontosCadastrados extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_pontos_cadastrados);
+    }
+}
